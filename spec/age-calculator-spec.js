57bf10a7c8e3acde8ca04dd49f1b383f0a8caca2
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-11 12:55:40 -0800

    Pass spec 7; calculate stats for each planet and return array

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -66,10 +66,12 @@ describe('AgeCalculator', function() {
   });
 
   describe('calculateStats', function() {
-    it('should store the user\'s age and life expectency for each planet', function() {
-      expect(user.calculateStats()).toEqual("[['Mercury','Age: 133.33333333333334, 'Years to live: 225.0000000000'], ['Venus','Age: 50, 'Years to live: 225.0000000000'], ['Mars','Age: 17.021276595744681, 'Years to live: 225.0000000000'], ['Jupiter','Age: 2.69814502529511, 'Years to live: 225.0000000000'] ");
+    it('should calculate the user\'s age and life expectency for each planet', function() {
+      expect(user.calculateStats()).toEqual([['Earth', 32, 86, 54],['Mercury', 133.33333333333334, 358.33333333333337, 225.00000000000003], ['Venus', 208.33333333333334, 559.8958333333334, 351.5625], ['Mars', 110.81560283687945, 297.8169326241135, 187.00132978723406], ['Jupiter', 9.343642734981405, 25.111039850262525, 15.76739711528112]]);
+
+      const user2 = new AgeCalculator(100, "female", "high", "healthy");
+      expect(user2.calculateStats()).toEqual([['Earth', 100, 86, "You've already lived 14 years longer than expected."],['Mercury', 416.6666666666667, 358.33333333333337, 'You\'ve already lived 58.333333333333314 years longer than expected.'], ['Venus', 651.0416666666666, 559.8958333333334, 'You\'ve already lived 91.14583333333326 years longer than expected.'], ['Mars', 346.2987588652482, 297.8169326241135, 'You\'ve already lived 48.481826241134684 years longer than expected.'], ['Jupiter', 29.198883546816884, 25.111039850262525, 'You\'ve already lived 4.0878436965543585 years longer than expected.']]);
+
     });
   });
-
-
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -4,6 +4,30 @@ export class AgeCalculator {
     this.lifeExpectency = this.calculateLifeExpectency(gender, activityLevel, lifestyle);
   }
 
+  calculateLifeExpectency(gender, activityLevel, lifestyle) {
+    let expectencyEarthYears = 75;
+    (gender === "female") ? expectencyEarthYears += 5 : expectencyEarthYears -=3;
+    (activityLevel === "high") ? expectencyEarthYears += 3 : expectencyEarthYears -= 5;
+    (lifestyle === "healthy") ? expectencyEarthYears += 3 : expectencyEarthYears -=2;
+    return expectencyEarthYears;
+  }
+
+  calculateStats(){
+    const planets = [['Earth', 1], ['Mercury', .24], ['Venus', .64], ['Mars', 1.88], ['Jupiter', 11.86]];
+    let results = [];
+    let age = this.ageInYears;
+    let lifeExpectency = this.lifeExpectency;
+    planets.forEach(function(planet) {
+      const planetName = planet[0];
+      age/=planet[1];
+      lifeExpectency/=planet[1];
+      let yearsToLive;
+      (age > lifeExpectency) ? (yearsToLive = `You've already lived ${age-lifeExpectency} years longer than expected.`) : (yearsToLive = lifeExpectency-age);
+      results.push([planetName, age, lifeExpectency, yearsToLive])
+    });
+    return results;
+  }
+
   mercuryYears(earthYears) {
     return earthYears/.24;
   }
@@ -19,15 +43,4 @@ export class AgeCalculator {
   jupiterYears(earthYears) {
     return earthYears/11.86;
   }
-
-  calculateLifeExpectency(gender, activityLevel, lifestyle) {
-    let lifeExpectencyEarthYears = 75;
-    (gender === "female") ? lifeExpectencyEarthYears += 5 : lifeExpectencyEarthYears -=3;
-    (activityLevel === "high") ? lifeExpectencyEarthYears += 3 : lifeExpectencyEarthYears -= 5;
-    (lifestyle === "healthy") ? lifeExpectencyEarthYears += 3 : lifeExpectencyEarthYears -=2;
-
-    return lifeExpectencyEarthYears;
-  }
-
-
 }
