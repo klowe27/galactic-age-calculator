45541a029baa2f84c228d7ae5bdab1a5ea792ecd
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-11 09:00:42 -0800

    Add spec 1; fails

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -3,7 +3,7 @@ import { AgeCalculator } from './../src/age-calculator.js';
 describe('AgeCalculator', function() {
 
   describe('constructor', function() {
-    it('should create a new instance of AgeCalculator and save the age property', function() {
+    it('should create a new instance of AgeCalculator and save the age (in years) property', function() {
       let userAge = new AgeCalculator(55);
       expect(userAge.age).toEqual(55);
     });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,5 +1,30 @@
+// Super Galactic Age Calculator
+// A solar year lasts 365 days on planet Earth. However, the length of a solar year varies from planet to planet. For instance, a solar year on Mercury lasts 88 days. Write an application that determines a user’s age based on a planet’s solar years. You will need to use the Javascript Date object.
+//
+// You are not required to build a UI for this application. Instead, focus on thoroughly testing each piece of functionality. Make sure to commit your specs before starting to code. You need at least one test for each spec.
+//
+// The business logic of your application should:
+//
+// Take a person’s age in years and create a class that does the following:
+//
+// Returns their age in Mercury years. (A Mercury year is .24 Earth years.)
+// if someone is 32 years old on earth. 32/.24 = 133.3333333 Mercury years.
+// Returns their age in Venus years. (A Venus year is .62 Earth years.)
+// Returns their age in Mars years. (A Mars year is 1.88 Earth years.)
+// Returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)
+// Determines how many years a user has left to live on each planet… (yikes!) To do this, the application will have to calculate the user’s life expectancy. You can determine average life expectancy as you see fit. A simple way to do this would be to have the user input the average life expectancy for their demographic. A more involved way to do this would be to collect other information from the user (e.g. lifestyle, country of residence, activity level, etc.) to determine their life expectancy.
+// If a user has already surpassed the average life expectancy, return the number of years they have lived past the life expectancy.
+// Further Exploration
+// If you're able to complete the project with time to spare, implement additional features such as:
+//
+// Add a UI to your application.
+// Return the date of a user’s next birthday on each planet.
+// Return the age Keith Richards will be in dog years on the planet Jupiter in 2073.
+// Assuming that the average lifespan of a mayfly is 5 minutes, determine how many mayfly lifespans a human user has lived and then compare it the current age (in Earth years) of our sun.
+// Make your application look nice and give it a solar theme!
+
 export class AgeCalculator {
-  constructor(age) {
-    this.age = age;
-  }
+  // constructor(age) {
+  //   this.age = age;
+  // }
 }
