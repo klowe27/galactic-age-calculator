2ac45c33cbc6424ff38b5ef8aeef5df67540beab
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-11 09:13:26 -0800

    Add spec 2; fails

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -1,11 +1,21 @@
 import { AgeCalculator } from './../src/age-calculator.js';
 
 describe('AgeCalculator', function() {
+  let user;
+
+  beforeEach(function() {
+    user = new AgeCalculator(32);
+  });
 
   describe('constructor', function() {
     it('should create a new instance of AgeCalculator and save the age (in years) property', function() {
-      let user = new AgeCalculator(55);
-      expect(user.ageInYears).toEqual(55);
+      expect(user.ageInYears).toEqual(32);
+    });
+  });
+
+  describe('ageInMercuryYears', function() {
+    it('should calculate the user\'s age in Mercury years', function() {
+      expect(user.ageInMercuryYears()).toEqual(133.33);
     });
   });
 });
