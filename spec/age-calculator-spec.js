b650367ca4849186b9af8c8e1b884c0c01f48d6d
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-11 10:00:51 -0800

    Add spec 5; fails

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -15,19 +15,25 @@ describe('AgeCalculator', function() {
 
   describe('ageInMercuryYears', function() {
     it('should calculate the user\'s age in Mercury years', function() {
-      expect(user.ageInMercuryYears()).toEqual('133.33');
+      expect(user.ageInMercuryYears()).toEqual(133.33333333333334);
     });
   });
 
   describe('ageInVenusYears', function() {
     it('should calculate the user\'s age in Venus years', function() {
-      expect(user.ageInVenusYears()).toEqual('50.00');
+      expect(user.ageInVenusYears()).toEqual(50);
     });
   });
 
   describe('ageInMarsYears', function() {
     it('should calculate the user\'s age in Mars years', function() {
-      expect(user.ageInMarsYears()).toEqual('17.02');
+      expect(user.ageInMarsYears()).toEqual(17.021276595744681);
+    });
+  });
+
+  describe('ageInJupiterYears', function() {
+    it('should calculate the user\'s age in Mars years', function() {
+      expect(user.ageInJupiterYears()).toEqual(2.758620689655172);
     });
   });
 
